08823c53fba94c5d414858e4a4bbbcfc39aaa927
Gabriel <gb_souza@id.uff.br>
2022-10-30 21:37:45 -0300

    add moving script

--- Monopoly/Assets/ballMovingScript.cs
+++ Monopoly/Assets/ballMovingScript.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ballMovingScript : MonoBehaviour
 {
 
-    int myInt = 5;
+    public Transform target; 
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(myInt * 2);
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        transform.position = target.position;
     }
 }
