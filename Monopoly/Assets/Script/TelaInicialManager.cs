4e04df048b2d2184ea178daa30b4bf6b773b41af
Bruno Speker <bruno.speker@gmail.com>
2022-11-22 10:01:45 -0300

    telaInicial 98% e HUDcenaPrincipal 50%

--- Monopoly/Assets/Script/TelaInicialManager.cs
+++ Monopoly/Assets/Script/TelaInicialManager.cs
@@ -14,56 +14,56 @@ public class TelaInicialManager : MonoBehaviour
     
     public void ModoSingle()
     {
-        IconeJogadores()
-	SceneManager.LoadScene(sceneIniciarJogo);
+        IconeJogadores();
+		//SceneManager.LoadScene(sceneIniciarJogo);
     }
 
     public void ModoMulti()
     {
-        QuantJogadores()
-	IconeJogadores()
+        QuantJogadores();
+		IconeJogadores();
 	//SceneManager.LoadScene(sceneIniciarJogo);
     }
 
     public void QuantJogadores()
     {
-        painelMenuPrincipal.setActive(false);
-	painelConfiguracoes.setActive(false);
-	painelQuantidadeJogadores.setActive(true);
-	painelIconeJogadores.setActive(false);
+        painelMenuPrincipal.SetActive(false);
+		painelConfiguracoes.SetActive(false);
+		painelQuantidadeJogadores.SetActive(true);
+		painelIconeJogadores.SetActive(false);
 	//Retornar a quantidade
     }
 
     public void IconeJogadores()
     {
         
-        painelMenuPrincipal.setActive(false);
-	painelConfiguracoes.setActive(false);
-	painelQuantidadeJogadores.setActive(false);
-	painelIconeJogadores.setActive(true);
+        painelMenuPrincipal.SetActive(false);
+		painelConfiguracoes.SetActive(false);
+		painelQuantidadeJogadores.SetActive(false);
+		painelIconeJogadores.SetActive(true);
 	//Retornar o icone escolhido
     }
 
     public void Configuracoes()
     {
-        painelMenuPrincipal.setActive(false);
-	painelConfiguracoes.setActive(true);
-	painelQuantidadeJogadores.setActive(false);
-	painelIconeJogadores.setActive(false);
+        painelMenuPrincipal.SetActive(false);
+		painelConfiguracoes.SetActive(true);
+		painelQuantidadeJogadores.SetActive(false);
+		painelIconeJogadores.SetActive(false);
     }
 
     public void Voltar()
     {
-        painelMenuPrincipal.setActive(true);
-	painelConfiguracoes.setActive(false);
-	painelQuantidadeJogadores.setActive(false);
-	painelIconeJogadores.setActive(false);
+        painelMenuPrincipal.SetActive(true);
+		painelConfiguracoes.SetActive(false);
+		painelQuantidadeJogadores.SetActive(false);
+		painelIconeJogadores.SetActive(false);
     }
 
     public void SairJogo()
     {
-	Debug.log("Sair do Jogo") //Remover quando compilar
-        Application.Quit()
+		Debug.Log("Sair do Jogo"); //Remover quando compilar
+        Application.Quit();
     }
 
 }
